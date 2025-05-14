c4108db9aa9b9aaf8ddddadaf47dc2660153dc53
prabakaranHpe <prabakaran.r@hpe.com>
2025-05-14 12:48:25 -0400

    logfile loc update

--- clear_docker_images.sh
+++ clear_docker_images.sh
@@ -1,4 +1,3 @@
 docker rmi -f campaign-optimizer-go-engine
 docker rmi -f campaign-optimizer-go-kafka-consumer
 docker rmi -f campaign-optimizer-go-generate_bids
-docker rmi -f prom/prometheus
--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -3,18 +3,28 @@ package utils
 import (
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
 
 func NewLogger(serviceName string) *logrus.Logger {
-	// Create log file with timestamp-based naming
-	logFileName := serviceName +  ".log"
-	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// Define the logs directory and file path
+	logDir := filepath.Join("..", "..", "logs")
+	logFileName := filepath.Join(logDir, serviceName + ".log")
+
+	// Create logs directory if it doesn't exist
+	if err := os.MkdirAll(logDir, 0777); err != nil {
+		panic("Unable to create log directory: " + err.Error())
+	}
+
+	// Create or open the log file
+	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
 	if err != nil {
 		panic("Unable to open log file: " + err.Error())
 	}
 
+	// Initialize logger
 	logger := logrus.New()
 
 	// Output to both file and stdout
@@ -23,7 +33,7 @@ func NewLogger(serviceName string) *logrus.Logger {
 
 	// Log format
 	logger.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp: true,
+		FullTimestamp:   true,
 		TimestampFormat: "2006-01-02 15:04:05",
 	})
 
