a890a5e8532cbdade49d760617cfba2d105f8aee
prabakaranHpe <prabakaran.r@hpe.com>
2025-05-14 10:23:01 -0400

    kafka docker setup

--- scripts/generate_bids.go
+++ scripts/generate_bids.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"campaign-optimization/internal/utils"
 	"campaign-optimization/internal/models"
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/rand"
 	"time"
+	"os"
 
 	"github.com/segmentio/kafka-go"
 )
 
 // This simulates the sample bidEvents for each event for every 500millisecond
 func main() {
+	logger := utils.NewLogger("generate_bids")
+	defer utils.RecoverAndLogPanic(logger)
+	// Set up Kafka reader config
+	kafkaHost := os.Getenv("KAFKA_HOST")
+	kafkaTopic := os.Getenv("KAFKA_TOPIC")
+
 	writer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"localhost:9092"},
-		Topic:   "test-topic",
+		Brokers: []string{kafkaHost},
+		Topic:   kafkaTopic,
 	})
 
 	defer writer.Close()
@@ -41,13 +48,13 @@ func main() {
 		if err != nil {
 			// Handle specific error
 			if err.Error() == "kafka server: Unknown Topic or Partition" {
-				log.Printf("Kafka error: topic or partition not found: %v", err)
+				logger.Errorf("Kafka error: topic or partition not found: %v", err)
 				// optionally: retry logic, topic creation, etc.
 			} else {
-				log.Printf("Kafka write failed: %v", err)
+				logger.Errorf("Kafka write failed: %v", err)
 			}
 		}
-
+		logger.Infof("Pushed to kafka: %v", bid)
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 	}
 
